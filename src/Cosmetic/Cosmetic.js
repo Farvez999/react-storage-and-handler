6f35cdb5265d0d4543d721f5032e4d4c2ecb62bf
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-24 11:12:36 +0600

    Careate Event handler with parameters

--- src/Cosmetic/Cosmetic.js
+++ src/Cosmetic/Cosmetic.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const { name, price } = props.cosmetic;
+    const { id, name, price } = props.cosmetic;
+
+    const addToCart = (id) => {
+        console.log('item added', id)
+    }
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this : {name}</h2>
             <h3>Only for : {price}$</h3>
+            <button onClick={() => addToCart(id)}>Add to cart</button>
         </div>
     );
 };
